10365ea75a9e6507a551bb761a70b03a69f4d83c
Arjita Arora <a.arjita1601@gmail.com>
2024-12-14 14:32:44 +0530

    Kafka ProducerAPI and Consumer

---
+++ KafkaAPIandConsumer/ProducerSevice.cs
@@ -0,0 +1,45 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaProducerAPI
+{
+    public class ProducerSevice
+    {
+        private ILogger<ProducerSevice> _logger;
+
+        public ProducerSevice(ILogger<ProducerSevice> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task ProduceAsync(CancellationToken cancellationToken)
+        {
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092",
+                AllowAutoCreateTopics = true,
+                Acks = Acks.All
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            try
+            {
+                var deliveryResult = await producer.ProduceAsync(topic: "test-topic",
+                    new Message<Null, string> { 
+                        Value = $"Hello Kafka!! {DateTime.UtcNow}" 
+                    },
+                    cancellationToken);
+
+                _logger.LogInformation($"Deivered Message to {deliveryResult.Value}, Offset: {deliveryResult.Offset}");
+
+            }
+            catch (ProduceException<Null,string>e)
+            {
+                _logger.LogInformation($"Delivery failed {e.Error.Reason}");
+            }
+
+            producer.Flush(cancellationToken);
+
+        }
+
+    }
+}
---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace KafkaConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start consuming events ...");
+
+            var builder = Host.CreateApplicationBuilder();
+
+            builder.Services.AddHostedService<EventConsumerJob>();
+
+            builder.Build().Run();
+        }
+    }
+}
